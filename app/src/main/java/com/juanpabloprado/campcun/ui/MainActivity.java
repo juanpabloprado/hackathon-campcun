9c2b271af19f77959aecf75d4af0aaefc8e84970
Juan Pablo Prado <juan@juanpabloprado.com>
2015-03-20 14:15:19 -0500

    Agregar clic listeners

--- app/src/main/java/com/juanpabloprado/campcun/ui/MainActivity.java
+++ app/src/main/java/com/juanpabloprado/campcun/ui/MainActivity.java
@@ -2,8 +2,17 @@ package com.juanpabloprado.campcun.ui;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.juanpabloprado.campcun.R;
+import com.juanpabloprado.campcun.model.TodoCamping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,5 +21,23 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+
+        List<TodoCamping> todoCampings = new ArrayList<>();
+    }
+
+    @OnClick(R.id.mapButton)
+    public void startMapActivity(View view) {
+        Toast.makeText(this, "Map", Toast.LENGTH_LONG).show();
+    }
+
+    @OnClick (R.id.direcotoryButton)
+    public void startDirectoryActivity(View view) {
+        Toast.makeText(this, "Directory", Toast.LENGTH_LONG).show();
+    }
+
+    @OnClick (R.id.todoButton)
+    public void startTodoActivity(View view) {
+        Toast.makeText(this, "Ready?", Toast.LENGTH_LONG).show();
     }
 }
