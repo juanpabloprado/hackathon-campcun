ac0f02e03124176b615145a533c7fa6e00efa487
Juan Pablo Prado <juan@juanpabloprado.com>
2015-03-21 03:07:17 -0500

    Agregar listeners de botones todo y directorio

--- app/src/main/java/com/juanpabloprado/campcun/ui/MapActivity.java
+++ app/src/main/java/com/juanpabloprado/campcun/ui/MapActivity.java
@@ -1,11 +1,14 @@
 package com.juanpabloprado.campcun.ui;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 
 import com.juanpabloprado.campcun.R;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MapActivity extends ActionBarActivity {
 
@@ -13,28 +16,18 @@ public class MapActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        ButterKnife.inject(this);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_map, menu);
-        return true;
+    @OnClick(R.id.todoButton)
+    public void startMainActivity(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    @OnClick(R.id.direcotoryButton)
+    public void startDirectoryActivity(View view) {
+        Intent intent = new Intent(this, DirectoryActivity.class);
+        startActivity(intent);
     }
 }
